96eca22c91341a9e36c47d1b3824d918a801e076
Pier Andrea <pierandreadelise@gmail.com>
2020-07-02 17:26:56 +0200

    feat: added components folder with homepage and game and two paths with react router in app

---
+++ src/components/Game/Game.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Game() {
+  return (
+    <div >
+      <p>I'm the Game </p>
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div >
+      <p>I'm the homepage </p>
+    </div>
+  );
+}
+
+export default Homepage;
