7de4410e5a32755b78509c8f2bea6c08c6d6d726
Pier Andrea <pierandreadelise@gmail.com>
2020-07-03 10:32:11 +0200

    feat: added new states to game component and a end of the game scenario + added gif(sources) file

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useParams} from 'react-router';
-import {play} from '../../logics/gameLogic'
+import {randomNumber, play} from '../../logics/gameLogic';
+import { Link } from "react-router-dom";
+
 
 function Game({match}) {
 
   const {points} = useParams();
-  return (
+  const [userScore, setUserScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [endOfGame, setEndOfGame] = useState();
+  const [userWins, setUserWins] = useState();
+  const [computerwins, setComputerWins] = useState();
+  const [disableButtons, setDisableButtons] = useState();
+
+  function playing (input) {
+    const pcInput = randomNumber(0, 3);
+    const result = play(pcInput, input);
+    console.log(result)
+    if (result==="win") {setUserScore(userScore+1)};
+    if (result==="lost") {setComputerScore(computerScore+1)};
+    return null;
+  }
+  
+  if(userScore===+points && !disableButtons) {
+    setDisableButtons(true);
+    setTimeout(()=> setEndOfGame(true),3000);
+    setUserWins(true);
+  }
+
+  if(computerScore===+points && !disableButtons) {
+    setDisableButtons(true);
+    setTimeout(()=> setEndOfGame(true),3000);
+    setComputerWins(true);
+  };
+
+  if (points>10) return (
     <div>
+      <p>Number of games too high, please go back to the homepage.</p>
+      <Link to={'/'}>
+        <button>We can help.</button>
+      </Link>
+    </div>
+  )
+
+  if(endOfGame) return (
+    <div>
+      <img src={computerwins? "https://media.giphy.com/media/7MezEc0TOaMlW/giphy.gif" : "https://media.giphy.com/media/cQNRp4QA8z7B6/giphy.gif"} alt="end of game"/>
+      {userWins && <p>You wins</p>}
+      {computerwins && <p>Computer wins</p>}
+      <p>Final score: You {userScore} vs Computer {computerScore}</p>
+    </div>
+  );
+
+  return (
+    <div style={{display:"disable"}}>
       <p>I'm the Game, we will play until {points}</p>
+      <p>Your Score: {userScore}</p>
+      <p>Computer Score: {computerScore}</p>
+      <button onClick={()=>playing(0)} disabled={disableButtons}>Rock</button>
+      <button onClick={()=>playing(1)} disabled={disableButtons}>Paper</button>
+      <button onClick={()=>playing(2)} disabled={disableButtons}>Scissor</button>
     </div>
   );
 }
+
 
 export default Game;
